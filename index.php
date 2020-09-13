5f0a9cdfabc92a617e2be27a7ce7c3bd870ee15b
Jarno Rankinen <t8raja03@students.oamk.fi>
2020-09-13 13:22:30 +0300

    Included the main web page php-file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+        <title>Lampotila</title>
+</head>
+<body>
+	<p><?php include 'current-temperature.html'; ?></p>
+	<p><a href="./history">Historia</a></p>
+	<p>Lämpötilat tänään:</p>
+	<p><img src="temperatures.png"></img></p>
+</body>
+
